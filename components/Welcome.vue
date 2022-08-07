675839fb75f4cd71ec29165f63f6d08474a75d34
Leo Clarke <leo@Leos-MacBook-Pro.local>
2022-08-07 20:56:43 +0100

    ui changes

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="min-h-screen bg-gray-100 ">
-    <div class="relative flex p-20">
-        <h1 class="text-5xl">Welcome </h1>
-        <span class="animate-wave text-5xl ml-2"> ✋🏻 </span>
+    <div class="relative flex px-28 pt-20">
+
+      <h1 class="text-7xl">Hey I'm Leo </h1>
+      <span class="animate-wave text-7xl ml-2"> ✋🏻 </span>
     </div>
+
+    <div class="relative flex px-28 pt-4">
+      <p class="text-2xl">Full Stack Software Developer</p>
+    </div>
+
   </div>
 </template>
 
@@ -12,3 +18,5 @@ export default {
   name: 'Welcome'
 }
 </script>
+
+<!--px-28 pt-12-->
